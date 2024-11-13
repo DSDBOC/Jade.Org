d41fc5c1890198ac1cdf7b1f33df6d13abde7bf4
Diamond Buchanan <winterdiamon@hotmail.com>
2024-11-12 21:43:12 -0500

    Add project files.

---
+++ JadeApplication/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JadeApplication.Startup))]
+namespace JadeApplication
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
